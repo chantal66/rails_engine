0c07c8e4eafa5518b1fab8060dfbee6d3d2b81e9
Desislava Todorova <dslavatodorova@gmail.com>
2017-08-14 23:32:43 -0600

    replace factory_girl gem, create item model

---
+++ spec/factories/items.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :item do
+    name "MyString"
+    description "MyText"
+    unit_price "MyString"
+  end
+end
--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -1,1 +1,11 @@
 require 'rails_helper'
+
+describe "Items API" do
+  it "sends a list of items" do
+    create_list(:item, 3)
+
+  get "/api/vi/items"
+
+  expect(response).to be_success
+  end
+end
