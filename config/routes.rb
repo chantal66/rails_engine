4478fcc89d088270a1117a20bbc45923ae3b5256
Desislava Todorova <dslavatodorova@gmail.com>
2017-08-16 16:39:51 -0600

    resolve conflicts and update development

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,37 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+
       namespace :items do
         get '/find_all', to: 'search#index'
         get '/find', to: 'search#show'
         get '/random', to: 'random#show'
       end
       resources :items, only: [:index, :show]
-      namespace :merchants do
-        get '/find_all', to: 'search#index'
-        get '/find', to: 'search#show'
-        get '/random', to: 'random#show'
-      end
-      resources :merchants, only: [:index, :show]
+
+      # namespace :merchants do
+      #   get '/find_all', to: 'search#index'
+      #   get '/find', to: 'search#show'
+      #   get '/random', to: 'random#show'
+      # end
+      # resources :merchants, only: [:index, :show]
+
       namespace :invoices do
         get '/find_all', to: 'search#index'
         get '/find', to: 'search#show'
         get '/random', to: 'random#show'
       end
       resources :invoices, only: [:index, :show]
+
+# tests not passing with correction on line 29; need to investigate
+      namespace :merchants do
+        get '/find', to: 'find#show'
+        get '/find_all', to: 'find_all#show'
+        get '/random', to: 'random#show'
+      end
+      resources :merchants, only: [:index, :show]
+      resources :transactions, only: [:index, :show]
+      resource :customers, only: [:index, :show]
     end
   end
 end
