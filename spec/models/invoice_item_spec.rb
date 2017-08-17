916493def6133b83dbcb7f8eb2a40c7eee5712ad
Desislava Todorova <dslavatodorova@gmail.com>
2017-08-17 16:53:12 -0600

    update syntax and add missing model tests

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe InvoiceItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "associations" do
+    it { is_expected.to respond_to(:invoice) }
+    it { is_expected.to respond_to(:item) }
+  end
+  describe "validations" do
+    it { should validate_presence_of(:quantity) }
+    it { should validate_presence_of(:unit_price) }
+    it { should validate_presence_of(:item_id) }
+    it { should validate_presence_of(:invoice_id) }
+  end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,21 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  describe "Validations" do
-
-    it "is created with attributes" do
-      merchant = create(:merchant)
-      item = create(:item, name: "Item", description: "description", merchant_id: merchant.id)
-
-      expect(item).to be_valid
-    end
-
-  describe "Associations" do
-    it "belongs to a merchant" do
-      item = create(:item)
-
-      expect(item).to respond_to(:merchant)
-    end
+  describe "associations" do
+    it { is_expected.to respond_to(:merchant) }
   end
-end
+  describe "validations" do
+    it { should validate_presence_of(:merchant_id) }
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:unit_price) }
+  end
 end
