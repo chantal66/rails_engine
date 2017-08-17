ff20848ba47f678fa1924569ba9f16acc8a09b32
Chantal Justamond <cjustamm66@me.com>
2017-08-16 20:59:54 -0600

    multiple a single finders working for customers, transactions

---
+++ spec/requests/api/v1/customers/random_request_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe 'Random customer API' do
+  context 'GET /api/v1/customers/random' do
+    it 'sends a random customer' do
+      create_list(:customer, 5)
+
+      get '/api/v1/customers/random'
+
+      expect(response).to be_success
+
+      raw_data = JSON.parse(response.body)
+
+      expect(raw_data).to have_key("id")
+    end
+  end
+end
