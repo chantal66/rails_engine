39e1df6af884c0e32663c4e04af720b4bc5b1575
Chantal Justamond <cjustamm66@me.com>
2017-08-17 21:50:21 -0600

    Item Best day test passing

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,22 +8,22 @@ class Item < ApplicationRecord
 
   def self.best_day(item_id)
     Invoice.joins(:invoice_items, :transactions)
-        .where(transactions: {result: 'success'}, invoice_items: {item_id: item_id})
-        .select("SUM(invoice_items.quantity) AS items_sold, invoices.created_at AS best_day")
-        .group("best_day")
-        .order("items_sold DESC, best_day DESC")
-        .limit(1).first
+      .where(transactions: {result: 'success'}, invoice_items: {item_id: item_id})
+      .select("SUM(invoice_items.quantity) AS items_sold, invoices.created_at AS best_day")
+      .group("best_day")
+      .order("items_sold DESC, best_day DESC")
+      .limit(1).first
   end
 
   def self.most_popular_items(quantity)
     Item.joins(
       "INNER JOIN (" +
         Invoice.joins(:invoice_items, :transactions)
-            .where(transactions: {result: 'success'})
-            .select("SUM(invoice_items.quantity) AS total_sold, invoice_items.item_id")
-            .group("item_id")
-            .order("total_sold DESC")
-            .limit(quantity).to_sql +
+          .where(transactions: {result: 'success'})
+          .select("SUM(invoice_items.quantity) AS total_sold, invoice_items.item_id")
+          .group("item_id")
+          .order("total_sold DESC")
+          .limit(quantity).to_sql +
         ") items_sold ON items_sold.item_id = items.id"
     )
   end
