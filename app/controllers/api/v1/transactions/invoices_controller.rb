ae31b89dd9cd0aeaf53652d610ac9f48fdf52d46
Desislava Todorova <dslavatodorova@gmail.com>
2017-08-18 07:42:34 -0600

    add invoices controller in transactions and relationship route in routes

---
+++ app/controllers/api/v1/transactions/invoices_controller.rb
@@ -0,0 +1,6 @@
+class Api::V1::Transactions::InvoicesController < ApplicationController
+
+  def show
+    render json: Transaction.find(params[:id]).invoice
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -48,6 +48,7 @@ Rails.application.routes.draw do
         get '/find', to: 'find#show'
         get '/find_all', to: 'find#index'
         get '/random', to: 'random#show'
+        get '/:id/invoice', to: 'invoices#show'
       end
 
       namespace :customers do
