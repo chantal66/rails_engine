e3c43fb82fc9fdfdcdf6f7de6de166bb910e8904
Chantal Justamond <cjustamm66@me.com>
2017-08-17 18:51:17 -0600

    favorite merchant passes test

--- spec/requests/api/v1/merchants/revenue_by_date_spec.rb
+++ spec/requests/api/v1/merchants/revenue_by_date_spec.rb
@@ -9,6 +9,7 @@ describe 'Merchants Revenue API' do
       invoice = create(:invoice, merchant_id: merchant.id, customer_id: customer.id, created_at: date)
       create(:transaction, invoice_id: invoice.id)
       create(:invoice_item, invoice_id: invoice.id, unit_price: 1025, quantity: 10)
+
       get "/api/v1/merchants/#{merchant.id}/revenue?date=#{date}"
 
       revenue = JSON.parse(response.body)
