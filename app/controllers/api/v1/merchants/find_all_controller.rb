9c00e4c92265320fc95bef9216d662fd13a70a9f
Chantal Justamond <cjustamm66@me.com>
2017-08-19 13:01:11 -0600

    returns all the first merchant it matches by update_at date FIXED typo

--- app/controllers/api/v1/merchants/find_all_controller.rb
+++ app/controllers/api/v1/merchants/find_all_controller.rb
@@ -7,6 +7,6 @@ class Api::V1::Merchants::FindAllController < ApplicationController
   private
 
   def merchant_params
-    params.permit(:id, :name, :created_at, :update_at)
+    params.permit(:id, :name, :created_at, :updated_at)
   end
 end
