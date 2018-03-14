126c52bf91a7ebbb684172f9c5fd20db8aec98a9
Chantal Justamond <cjustamm66@me.com>
2018-03-14 16:33:01 -0600

    removed pry from test

--- spec/requests/api/v1/customers/favorite_customer_spec.rb
+++ spec/requests/api/v1/customers/favorite_customer_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe "Merchants/Favorite Customer API" do
       get "/api/v1/merchants/#{merchant.id}/favorite_customer"
       expect(response).to be_success
       raw_customer = JSON.parse(response.body)
-      binding.pry
+
       expect(raw_customer).to have_key("id")
       expect(raw_customer["id"]).to be_a Integer
       expect(raw_customer["id"]).to eq(invoice2.customer.id)
